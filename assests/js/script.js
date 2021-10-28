e7c7cc5eb2eeacf0690b108402b9815dced98957
María José Bareño <47755417+mariajosebare@users.noreply.github.com>
2021-10-27 21:53:11 -0300

    login
    Culminacion de login con html,css,js

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,63 @@
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
+window.addEventListener("resize", anchodePagina);
+
 
 //Se pasa a declarar variables
 var contenedor_login_register = document.querySelector(".contenedor__login-register");
 var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__login");
-var caja_trasera_login = document.querySelector(".caja_trasera-login");
-var caja_trasera_register = document.querySelector(".caja_trasera-register");
+var formulario_register = document.querySelector(".formulario__register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
+
+/* Funcion para hacer responsive si la pantalla es mayor a 850*/
+function anchodePagina() {
+    if (window.innerWidth > 850) {
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+    } else {
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style = "0px";
+    }
+}
+
+anchodePagina();
+
+function iniciarSesion() {
+    if (window.innerWidth > 850) {
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
+    } else {
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display = "none";
+    }
+}
 
 function register() {
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
+    if (window.innerWidth > 850) {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    } else {
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "none";
+        caja_trasera_login.style.opacity = "block";
+        caja_trasera_login.style.opacity = "1"
+    }
+
+
 }
